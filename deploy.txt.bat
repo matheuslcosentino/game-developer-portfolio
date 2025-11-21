9e9710c8feb438c6100a91ffb430f7185545ae71
Matheus Lopes <matheuslopescosentino@hotmail.com>
2025-11-21 16:38:33 -0300

    Deploy automatico

--- deploy.txt.bat
+++ deploy.txt.bat
@@ -1,7 +1,7 @@
 git init
 git branch -m main
 git remote remove origin >nul 2>&1
-git remote add origin https://github.com/matheuslcosentino/game-developer-portfolio.git
+git remote add origin https://github.com/matheuslcosentino/matheuslcosentino.github.io.git
 git add -A
 git commit -m "Deploy automatico"
 git push -u origin main --force
